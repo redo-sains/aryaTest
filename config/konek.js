7f6ea1b6452f3566192d1c118c09d4529be97db4
arya205 <syarifrahmanbego@gmail.com>
2025-04-11 22:49:17 +0700

    Update isi project

--- config/konek.js
+++ config/konek.js
@@ -1,8 +1,15 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config(); 
 
-const db = new Sequelize('u443307867_inventaris', 'u443307867_root', '|qQl#R[ehWv3', {
+const db = new Sequelize(
+    'u443307867_inventaris',
+    'u443307867_root',
+  '5i+j$?HK0Jb&',
+  {
     host: 'srv1758.hstgr.io',
-    dialect: 'mysql'
-})
+    dialect: "mysql",
+  }
+);
 
-export default db
+export default db;
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-import express from "express"
-import cors from "cors"
-import path from "path"; 
-import penggunaRoute from "./route/penggunaRoute.js"
-import barangRoute from "./route/barangRoute.js"
-import barangMasukRoute from "./route/barangMasukRoute.js"
-import transaksiRoute from "./route/transaksiRoute.js"
+import express from "express";
+import cors from "cors";
+import path from "path";
+import penggunaRoute from "./route/penggunaRoute.js";
+import barangRoute from "./route/barangRoute.js";
+import barangMasukRoute from "./route/barangMasukRoute.js";
+import transaksiRoute from "./route/transaksiRoute.js";
+import serverless from "serverless-http";
 
-const app = express()
+const PORT = process.env.PORT || 5000
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 app.use('/gambar', express.static(path.resolve('gambar'))); 
 
-app.use(penggunaRoute)
-app.use(barangRoute)
-app.use(barangMasukRoute)
-app.use(transaksiRoute)
+app.use(penggunaRoute);
+app.use(barangRoute);
+app.use(barangMasukRoute);
+app.use(transaksiRoute);
 
-app.listen(5000, ()=>{
-    console.log("Web berjalan di port 5000");
-})
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+
+// export const handler = serverless(app); 
